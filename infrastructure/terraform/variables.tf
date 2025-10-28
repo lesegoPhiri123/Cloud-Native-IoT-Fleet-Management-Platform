408fec692149496b41c8ea23ee2cf207931dc2e8
leseg <lesegophiri745@gmail.com>
2025-10-29 00:01:28 +0200

    create variables for terraform

--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -1,1 +1,17 @@
 # Variables for GKE cluster name, project ID, region, and MongoDB Atlas credentials.
+variable "project_id"{
+  description = "The ID of the GDP project"
+  type        = string
+}
+
+variable "region" {
+  description = "the GDP region to deploy resources"
+  type = string
+  default = "us-central1"
+}
+
+variable "cluster_name" {
+  description = "The name of the Kubernetes cluster"
+  type = string
+  default = "iot-fleet-cluster"
+}
